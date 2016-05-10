67653c112c4d90b62852d624903ef8fdc58ac4d1
Gris87 <Gris87@yandex.ru>
2016-05-10 17:02:41 +0300

    Prepare UI

--- app/src/main/java/ru/okmarket/okgoods/activities/MainActivity.java
+++ app/src/main/java/ru/okmarket/okgoods/activities/MainActivity.java
@@ -1,16 +1,47 @@
 package ru.okmarket.okgoods.activities;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import ru.okmarket.okgoods.R;
+import ru.okmarket.okgoods.adapters.PagerAdapter;
+import ru.okmarket.okgoods.fragments.GoodsFragment;
+import ru.okmarket.okgoods.fragments.ShopMapFragment;
 
 public class MainActivity extends AppCompatActivity
 {
+    private ViewPager       mPager           = null;
+    private PagerAdapter    mPagerAdapter    = null;
+    private GoodsFragment   mGoodsFragment   = null;
+    private ShopMapFragment mShopMapFragment = null;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        mPager = (ViewPager)findViewById(R.id.pager);
+
+        if (mPager != null)
+        {
+            mPagerAdapter = new PagerAdapter(fragmentManager);
+
+            mPager.setAdapter(mPagerAdapter);
+
+            mGoodsFragment   = (GoodsFragment)   mPagerAdapter.getItem(PagerAdapter.PAGE_GOODS);
+            mShopMapFragment = (ShopMapFragment) mPagerAdapter.getItem(PagerAdapter.PAGE_SHOP_MAP);
+        }
+        else
+        {
+            mGoodsFragment   = (GoodsFragment)   fragmentManager.findFragmentById(R.id.goodsFragment);
+            mShopMapFragment = (ShopMapFragment) fragmentManager.findFragmentById(R.id.shopMapFragment);
+        }
     }
 }
---
+++ app/src/main/java/ru/okmarket/okgoods/adapters/PagerAdapter.java
@@ -0,0 +1,43 @@
+package ru.okmarket.okgoods.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import ru.okmarket.okgoods.fragments.GoodsFragment;
+import ru.okmarket.okgoods.fragments.ShopMapFragment;
+
+public class PagerAdapter extends FragmentPagerAdapter
+{
+    public  static final int PAGE_GOODS    = 0;
+    public  static final int PAGE_SHOP_MAP = 1;
+    private static final int PAGES_COUNT   = 2;
+
+
+
+    private Fragment[] mFragments;
+
+
+
+    public PagerAdapter(FragmentManager fragmentManager)
+    {
+        super(fragmentManager);
+
+        mFragments = new Fragment[PAGES_COUNT];
+
+        mFragments[PAGE_GOODS]    = new GoodsFragment();
+        mFragments[PAGE_SHOP_MAP] = new ShopMapFragment();
+    }
+
+    @Override
+    public Fragment getItem(int position)
+    {
+        return mFragments[position];
+    }
+
+    @Override
+    public int getCount()
+    {
+        return PAGES_COUNT;
+    }
+}
