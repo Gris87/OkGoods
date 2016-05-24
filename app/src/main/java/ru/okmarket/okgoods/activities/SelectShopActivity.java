7cec12bafe513f9702747d0af25939b6d8986158
Gris87 <Gris87@yandex.ru>
2016-05-24 18:04:54 +0300

    Small fix

--- app/src/main/java/ru/okmarket/okgoods/activities/SelectShopActivity.java
+++ app/src/main/java/ru/okmarket/okgoods/activities/SelectShopActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import ru.okmarket.okgoods.R;
+import ru.yandex.yandexmapkit.MapView;
 
 public class SelectShopActivity extends AppCompatActivity
 {
     private static final String TAG = "SelectShopActivity";
+
+
+
+    private MapView mMapView;
 
 
 
@@ -16,5 +21,9 @@ public class SelectShopActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_shop);
+
+        mMapView = (MapView)findViewById(R.id.map);
+
+        mMapView.showJamsButton(true);
     }
 }
