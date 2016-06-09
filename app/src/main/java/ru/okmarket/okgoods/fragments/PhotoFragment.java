5f0f6019b5117001cc722f6e526b4271f3aa0eda
Gris87 <Gris87@yandex.ru>
2016-06-09 18:02:32 +0300

    Display photo

--- app/src/main/java/ru/okmarket/okgoods/fragments/PhotoFragment.java
+++ app/src/main/java/ru/okmarket/okgoods/fragments/PhotoFragment.java
@@ -1,13 +1,18 @@
 package ru.okmarket.okgoods.fragments;
 
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.toolbox.ImageLoader;
+
 import ru.okmarket.okgoods.R;
+import ru.okmarket.okgoods.net.HttpClient;
+import uk.co.senab.photoview.PhotoView;
 
 public class PhotoFragment extends Fragment
 {
@@ -56,6 +61,11 @@ public class PhotoFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View rootView = inflater.inflate(R.layout.fragment_photo, container, false);
+
+        PhotoView photoView = (PhotoView)rootView.findViewById(R.id.photoView);
+        ImageLoader imageLoader = HttpClient.getInstance(getActivity()).getImageLoader();
+
+        imageLoader.get(mUrl, ImageLoader.getImageListener(photoView, R.drawable.ymk_ya_logo, R.drawable.delete));
 
         return rootView;
     }
