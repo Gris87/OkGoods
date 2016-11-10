229129ff0ad4c4e8d204c116e84ba7cbf88e28f1
Gris87 <Gris87@yandex.ru>
2016-11-10 11:52:12 +0300

    Updated to Android Studio 2.2.2

---
+++ app/src/main/java/ru/okmarket/okgoods/OkGoodsApplication.java
@@ -0,0 +1,50 @@
+package ru.okmarket.okgoods;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
+/**
+ * OkGoods application
+ */
+@SuppressWarnings({"ClassWithoutConstructor", "PublicConstructor"})
+public class OkGoodsApplication extends Application
+{
+    @SuppressWarnings("unused")
+    private static final String TAG = "OkGoodsApplication";
+
+
+
+    private Tracker mTracker = null;
+
+
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "OkGoodsApplication{" +
+                "mTracker=" + mTracker +
+                '}';
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+        mTracker = GoogleAnalytics.getInstance(this).newTracker(R.xml.tracker_config);
+    }
+
+    /**
+     * Gets the default {@link Tracker} for this {@link Application}.
+     *
+     * @return Google Analytics tracker
+     */
+    public Tracker getDefaultTracker()
+    {
+        return mTracker;
+    }
+}
