005d6a69612a501965861c3ac3d2dbaac8295917
Gris87 <Gris87@yandex.ru>
2016-11-10 12:32:52 +0300

    Small fix

--- app/src/androidTest/java/ru/okmarket/okgoods/ApplicationTest.java
+++ app/src/androidTest/java/ru/okmarket/okgoods/ApplicationTest.java
@@ -6,8 +6,14 @@ import android.test.ApplicationTestCase;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+@SuppressWarnings("PublicConstructor")
 public class ApplicationTest extends ApplicationTestCase<Application>
 {
+    @SuppressWarnings("unused")
+    private static final String TAG = "ApplicationTest";
+
+
+
     public ApplicationTest()
     {
         super(Application.class);
--- app/src/test/java/ru/okmarket/okgoods/ExampleUnitTest.java
+++ app/src/test/java/ru/okmarket/okgoods/ExampleUnitTest.java
@@ -7,8 +7,14 @@ import static org.junit.Assert.assertEquals;
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+@SuppressWarnings("PublicConstructor")
 public class ExampleUnitTest
 {
+    @SuppressWarnings("unused")
+    private static final String TAG = "ExampleUnitTest";
+
+
+
     @Test
     public void addition_isCorrect() throws Exception
     {
