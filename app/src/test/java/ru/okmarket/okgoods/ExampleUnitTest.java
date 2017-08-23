5374c493f1724af8fcb2d0ed959d156dd62d8243
Gris87 <Gris87@yandex.ru>
2017-08-23 12:48:48 +0300

    Project updated to the latest Android Studio

--- app/src/test/java/ru/okmarket/okgoods/ExampleUnitTest.java
+++ app/src/test/java/ru/okmarket/okgoods/ExampleUnitTest.java
@@ -4,7 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @SuppressWarnings({"PublicConstructor", "JUnitTestNG", "ClassWithoutConstructor"})
 public class ExampleUnitTest
